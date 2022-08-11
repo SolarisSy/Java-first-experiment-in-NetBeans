bb43f8510a60ed1e84045b8997f065a1c9531ad5
NickError404 <nicklink.lv@gmail.com>
2022-08-11 17:09:19 -0300

    First commit in Java

---
+++ README.md
@@ -0,0 +1,1 @@
+# Java-first-experiment-in-NetBeans
---
+++ src/Controller/Helper/IHelper.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller.Helper;
+
+
+
+
+/**
+ *
+ * @author tiago
+ */
+public interface IHelper {
+    
+    public abstract Object obterModelo();
+        
+    public abstract void limparTela();
+    
+    
+}
